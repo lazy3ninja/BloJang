d86742440733dab935650048c133cdd0d34567f2
Alex <aatmanupreti@gmail.com>
2024-04-20 01:45:07 +0530

    second_commit

--- BloJang/users/views.py
+++ BloJang/users/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import UserRegisterForm
 
 class RegisterView(View):
-    def get(self,request):
-        pass
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
 
-    def post(self,request):
-        pass
-    
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirect to a different URL upon successful registration
+        else:
+            # Handle invalid form submission here
+            return render(request, 'users/register.html', {'form': form})
